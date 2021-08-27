11bcbff2ee0b592ec1686a9fca5fb9cb128aa20e
Wesley Israel <wesley12israel@gmail.com>
2021-08-27 02:02:39 -0300

    feat(finished): finished Watchme ignite

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+
+import { Button } from "./Button";
+
+import "../styles/sidebar.scss";
+import { api } from "../services/api";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface Sidebar {
+  onClickButton(id: number): void;
+  selected: number;
+}
+
+export function SideBar({ onClickButton, selected }: Sidebar) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickButton(genre.id)}
+            selected={selected === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
